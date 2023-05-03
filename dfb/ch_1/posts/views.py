0b9683a52d4ce7d9b3af91eb5b7a9540dea5b453
mmrubayet <rubayet.wrk@gmail.com>
2023-05-03 16:38:30 +0600

    Blog post with detail page

--- dfb/ch_1/posts/views.py
+++ dfb/ch_1/posts/views.py
@@ -1,6 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+
 
 class PostListView(ListView):
     model = Post
     template_name = "posts/list.html"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/detail.html"
