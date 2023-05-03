5cc2fc31992b7308d757ad957b4d395c57672498
Rubayet Mahmud <rubayet.mahmud57@gmail.com>
2023-05-03 11:50:19 +0600

    blog post with body and css

--- dfb/ch_1/posts/models.py
+++ dfb/ch_1/posts/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-class Post(models.Model):  # new
-    text = models.TextField()
 
-    def __str__(self) -> str:
-        return self.text[:50]
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title[:80]
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
