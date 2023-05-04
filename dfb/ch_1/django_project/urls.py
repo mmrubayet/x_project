4570b73ac68bb1f4e151371904431c3a57efcd9d
mmrubayet <rubayet.wrk@gmail.com>
2023-05-04 17:02:03 +0600

    login and logout

--- dfb/ch_1/django_project/urls.py
+++ dfb/ch_1/django_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("pages.urls")),
     path("post/", include("posts.urls")),
 ]
