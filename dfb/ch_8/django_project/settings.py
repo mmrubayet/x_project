a86047b650274aeecbece058e6ea2edd920874a0
mmrubayet <rubayet.wrk@gmail.com>
2023-05-10 16:30:58 +0600

    bootstrap added

--- dfb/ch_8/django_project/settings.py
+++ dfb/ch_8/django_project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # project apps
     "accounts.apps.AccountsConfig",
+    "pages.apps.PagesConfig",
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +133,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# settings for crispy
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
