a5cf3e9160a99c259e475e310e71db217ebfe35e
Rubayet Mahmud <rubayet.mahmud57@gmail.com>
2023-05-12 11:36:47 +0600

    articles CRUD in article app

--- dfb/ch_8/django_project/settings.py
+++ dfb/ch_8/django_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     # project apps
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
+    "articles.apps.ArticlesConfig",
     # 3rd Party
     "crispy_forms",
     "crispy_bootstrap5",
@@ -118,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Dhaka"
 
 USE_I18N = True
 
