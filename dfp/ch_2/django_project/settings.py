4bd6db4309a2e0645a25d827c7f73bf7a5edb00c
Rubayet Mahmud <rubayet.mahmud57@gmail.com>
2023-05-23 11:42:53 +0600

    custom user with accounts app

--- dfp/ch_2/django_project/settings.py
+++ dfp/ch_2/django_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # project apps
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
