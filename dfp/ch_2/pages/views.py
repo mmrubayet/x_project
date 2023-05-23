90d3d368b16d962aa54744ffddd316062f945855
mmrubayet <rubayet.wrk@gmail.com>
2023-05-23 19:33:05 +0600

    updated pages app with CBV and test

--- dfp/ch_2/pages/views.py
+++ dfp/ch_2/pages/views.py
@@ -1,6 +1,7 @@
 # pages/views.py
-from django.http import HttpResponse
+
+from django.views.generic import TemplateView
 
 
-def home_page_view(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+    template_name = "home.html"
