2e7619e39bc53469373a8f709a94340ba930ac6e
mmrubayet <rubayet.wrk@gmail.com>
2023-05-29 18:22:33 +0600

    update test cases

--- dfp/ch_2/accounts/tests.py
+++ dfp/ch_2/accounts/tests.py
@@ -1,13 +1,17 @@
 # accounts/tests.py
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-        username="will", email="will@email.com", password="testpass123"
+        user = get_user_model()
+        user = user.objects.create_user(
+            username="will", email="will@email.com", password="testpass123"
         )
         self.assertEqual(user.username, "will")
         self.assertEqual(user.email, "will@email.com")
@@ -16,9 +20,9 @@ class CustomUserTests(TestCase):
         self.assertFalse(user.is_superuser)
 
     def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser(
-        username="superadmin", email="superadmin@email.com", password="testpass123"
+        user = get_user_model()
+        admin_user = user.objects.create_superuser(
+            username="superadmin", email="superadmin@email.com", password="testpass123"
         )
         self.assertEqual(admin_user.username, "superadmin")
         self.assertEqual(admin_user.email, "superadmin@email.com")
