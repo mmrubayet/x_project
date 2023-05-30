85cbfc2ce43e3319ecb12760b884f0d6050d5f1b
mmrubayet <rubayet.wrk@gmail.com>
2023-05-30 13:31:37 +0600

    static assets, ch7

--- dfp/ch_2/django_project/settings.py
+++ dfp/ch_2/django_project/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     # project apps
     'pages.apps.PagesConfig',
     'accounts.apps.AccountsConfig',
+    # third-party apps
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -122,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -133,3 +139,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# django-crispy-forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
